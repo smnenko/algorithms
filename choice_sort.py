273e52f685fb771411ac1875f10c9b8556807957
Stanislau Semianenka <stanislausemianenka@wsa-043-22a.icx.local>
2021-11-25 10:10:20 +0300

    choice sort vs quick sort comparison

--- choice_sort.py
+++ choice_sort.py
@@ -1,3 +1,4 @@
+import math
 from random import randrange
 from time import time
 
@@ -8,6 +9,8 @@ start_number = 1
 stop_number = 100
 numbers_count = 5_000
 lst = [randrange(start_number, stop_number) for i in range(numbers_count)]
+lst_copy = lst[:]
+k = 0
 
 
 def get_smallest_index(unsorted_list: list):
@@ -22,14 +25,52 @@ def get_smallest_index(unsorted_list: list):
 
 
 def choice_sort(unsorted_list: list):
+    k = 0
     _start = time()
     sorted_list = []
     for i in range(len(unsorted_list)):
         smallest_item = get_smallest_index(unsorted_list)
         sorted_list.append(unsorted_list.pop(smallest_item))
-    print_statistics('Choice sort', time() - _start, 0)
+        k += 2
+    print_statistics('Choice sort', time() - _start, k)
     return sorted_list
+
+
+def quick_sort(unsorted_list: list):
+    global k
+    if len(unsorted_list) < 2:
+        return unsorted_list
+    else:
+        avg_index = math.ceil(len(unsorted_list) / 2)
+        base_num = unsorted_list[avg_index]
+        less_num = [
+            i
+            for i in unsorted_list[1:avg_index] + unsorted_list[avg_index:]
+            if i <= base_num
+        ]
+        greater_num = [
+            i
+            for i in unsorted_list[1:avg_index] + unsorted_list[avg_index:]
+            if i > base_num
+        ]
+        k += 2
+        return quick_sort(less_num) + [base_num] + quick_sort(greater_num)
+
+
+def call_quick_sort(unsorted_list: list):
+    _start = time()
+    quick_sort(unsorted_list)
+    print_statistics('Quick sort', time() - _start, k)
+
+
+def python_sort(unsorted_list: list):
+    _start = time()
+    result = sorted(unsorted_list)
+    print_statistics('Python sort', time() - _start, 0)
+    return result
 
 
 if __name__ == '__main__':
     choice_sort(lst)
+    call_quick_sort(lst_copy)
+    python_sort(lst_copy)
